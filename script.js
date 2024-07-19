09f9ce2555f0cfd76a2359a9fbac7a0c803eafac
Santiago Daniel <98777824+dannfront@users.noreply.github.com>
2024-07-19 15:15:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict'
+const slides = document.querySelectorAll(".slide-js")
+const next = document.querySelector(".button-1")
+const prev = document.querySelector(".button-2")
+
+let steps = 0
+
+let x = true
+
+function slideNext() {
+    slides.forEach(slide => {
+        slide.style.display = "none"
+    })
+    ++steps
+    if (steps === slides.length) steps = 0
+
+    slides[steps].style.display = "block"
+}
+
+
+function slidePrev() {
+    slides.forEach(slide => {
+        slide.style.display = "none"
+    })
+    --steps
+    if (steps < 0) steps = slides.length - 1
+    slides[steps].style.display = "block"
+    console.log(steps); 
+}
+
+prev.addEventListener('click', slidePrev)
+next.addEventListener('click', slideNext)
